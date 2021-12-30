8b244df1ff47e497dc6103d59dee38280dd1d24f
TomVarghese2468 <96836715+TomVarghese2468@users.noreply.github.com>
2021-12-30 21:47:22 +0530

    Create Fibonacci.C

---
+++ Fibonacci.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int n,i,p,q,r;
+printf("Enter the Limit:");
+scanf("%d",&n);
+p=0;
+q=1;
+for(i=1;i<=n;i++)
+   {
+      printf("%d\t",p);
+      r=p+q;
+      p=b;
+      q=r;
+    }
+return 0;
+}
