5d581f907a09601630f213ac9917d96883ed13a0
TomVarghese2468 <96836715+TomVarghese2468@users.noreply.github.com>
2021-12-30 21:32:46 +0530

    Create Palindrome.C

---
+++ Palindrome.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int n,r,sum=0,temp;
+	printf("Enter the number ");
+	scanf("%d",&n);
+	temp=n;
+	while(n>0)
+	{
+		r=n%10;
+		sum=sum*10+r;
+		n=n/10;
+	}
+	if(temp==sum)
+		printf("%d Is a Palindrome number",temp);
+	else
+		printf("%d Is not a Palindrome number",temp);
+        return 0;
+        }
+}
